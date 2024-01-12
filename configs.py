38ea15f5695d15bfb14e5f5d9e11fde81afbd35a
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2024-01-12 13:31:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(getenv("API_ID", "9634153"))
     API_HASH = getenv("API_HASH", "67bd6a23278a884e575a6ebf15b0e6bc")
     BOT_TOKEN = getenv("BOT_TOKEN", "6580703820:AAGx5M4N18Lt1Eukp_D2Sl8BLmCQiTZWksY")
-    FSUB = getenv("FSUB", "NivasBots")
-    CHID = int(getenv("CHID", "-1002139087930"))
+    FSUB = getenv("FSUB", "GM_Botzz")
+    CHID = int(getenv("CHID", "-1001848189155"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Nivas4:Nivas4@cluster0.xbn5smt.mongodb.net/?retryWrites=true&w=majority")
     
